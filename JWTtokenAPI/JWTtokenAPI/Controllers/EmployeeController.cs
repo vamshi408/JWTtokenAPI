bf9c5d1c50389d732a3cfb91deac36fb2b7f9742
vamshi408 <bandi.vamshi408@gmail.com>
2023-12-30 16:43:04 +0530

    Authorize attribute & API accessing using token based authorization

--- JWTtokenAPI/JWTtokenAPI/Controllers/EmployeeController.cs
+++ JWTtokenAPI/JWTtokenAPI/Controllers/EmployeeController.cs
@@ -1,10 +1,12 @@
 ﻿using JWTtokenAPI.Interfaces;
 using JWTtokenAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWTtokenAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeeController : ControllerBase
